85de82af47c7dca9ca46c1ae8cade23ff2fd17dc
Feng <ery.lee@gmail.com>
2015-10-31 13:48:03 +0800

    refactor client, proto

--- src/emqttd_stomp_client.erl
+++ src/emqttd_stomp_client.erl
@@ -24,12 +24,13 @@
 %%%
 %%% @end
 %%%-----------------------------------------------------------------------------
-
 -module(emqttd_stomp_client).
 
 -author("Feng Lee <feng@emqtt.io>").
 
 -include("emqttd_stomp.hrl").
+
+-include("../../../include/emqttd_internal.hrl").
 
 %% API Function Exports
 -export([start_link/2, info/1]).
@@ -40,103 +41,131 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          code_change/3, terminate/2]).
 
--record(state, {transport,
-                socket,
-                peername,
-                conn_name,
-                await_recv,
-                conn_state,
-                conserve,
-                parser,
-                proto_state,
-                proto_env,
-                heartbeat}).
+-record(stomp_client, {connection, peername, peerhost, peerport,
+                       await_recv, conn_state, rate_limit,
+                       parser_fun, proto_state, proto_env,
+                       heartbeat}).
 
-start_link(SockArgs, ProtoEnv) ->
-    {ok, proc_lib:spawn_link(?MODULE, init, [[SockArgs, ProtoEnv]])}.
+-define(INFO_KEYS, [peername, peerhost, peerport, await_recv, conn_state]).
+
+-define(SOCK_STATS, [recv_oct, recv_cnt, send_oct, send_cnt]).
+
+-define(LOG(Level, Format, Args, State),
+            lager:Level("Stomp(~s): " ++ Format, [State#stomp_client.peername | Args])).
+
+
+start_link(Connection, ProtoEnv) ->
+    {ok, proc_lib:spawn_link(?MODULE, init, [[Connection, ProtoEnv]])}.
 
 info(CPid) ->
     gen_server:call(CPid, info, infinity).
 
-init([SockArgs = {Transport, Sock, _SockFun}, ProtoEnv]) ->
+init([Connection0, ProtoEnv]) ->
     process_flag(trap_exit, true),
-    % Transform if ssl.
-    {ok, NewSock} = esockd_connection:accept(SockArgs),
-    {ok, Peername} = emqttd_net:peername(Sock),
-    {ok, ConnStr} = emqttd_net:connection_string(Sock, inbound),
-    lager:info("Connect from ~s", [ConnStr]),
-    SendFun = fun(Data) -> Transport:send(NewSock, Data) end,
-    ProtoState = emqttd_stomp_proto:init(Peername, SendFun, ProtoEnv),
-    State = control_throttle(#state{transport    = Transport,
-                                    socket       = NewSock,
-                                    peername     = Peername,
-                                    conn_name    = ConnStr,
-                                    await_recv   = false,
-                                    conn_state   = running,
-                                    conserve     = false,
-                                    proto_env    = ProtoEnv,
-                                    parser       = emqttd_stomp_frame:parser(ProtoEnv),
-                                    proto_state  = ProtoState}),
+    {ok, Connection} = Connection0:wait(),
+    {PeerHost, PeerPort, PeerName} =
+    case Connection:peername() of
+        {ok, {Host, Port}} ->
+            {Host, Port, esockd_net:format({Host, Port})};
+        {error, enotconn} ->
+            Connection:fast_close(),
+            exit(normal);
+        {error, Reason} ->
+            Connection:fast_close(),
+            exit({shutdown, Reason})
+    end,
+    SendFun = fun(Data) ->
+        try Connection:async_send(Data) of
+            true -> ok
+        catch
+            error:Error -> exit({shutdown, Error})
+        end
+    end,
+    ParserFun = emqttd_stomp_frame:parser(ProtoEnv),
+    ProtoState = emqttd_stomp_proto:init(PeerName, SendFun, ProtoEnv),
+    RateLimit = proplists:get_value(rate_limit, Connection:opts()),
+    State = run_socket(#stomp_client{connection   = Connection,
+                                     peername     = PeerName,
+                                     peerhost     = PeerHost,
+                                     peerport     = PeerPort,
+                                     await_recv   = false,
+                                     conn_state   = running,
+                                     rate_limit   = RateLimit,
+                                     parser_fun   = ParserFun,
+                                     proto_env    = ProtoEnv,
+                                     proto_state  = ProtoState}),
     gen_server:enter_loop(?MODULE, [], State, 10000).
 
-handle_call(info, _From, State = #state{conn_name = ConnName,
-                                        proto_state = ProtoState}) ->
-    Info = [{conn_name, ConnName} | emqttd_stomp_proto:info(ProtoState)],
-    {reply, Info, State};
+handle_call(info, _From, State = #stomp_client{connection  = Connection,
+                                               proto_state = ProtoState}) ->
 
-handle_call(_Req, _From, State) ->
+    ClientInfo = ?record_to_proplist(stomp_client, State, ?INFO_KEYS),
+    ProtoInfo  = emqttd_stomp_proto:info(ProtoState),
+    {ok, SockStats} = Connection:getstat(?SOCK_STATS),
+    {noreply, lists:append([ClientInfo, [{proto_info, ProtoInfo},
+                                         {sock_stats, SockStats}]]), State};
+
+handle_call(Req, _From, State) ->
+    ?LOG(critical, "Unexpected request: ~p", [Req], State),
+    
     {reply, {error, unsupported_request}, State}.
 
-handle_cast(Msg, State = #state{peername = Peername}) ->
-    lager:critical("Stomp(~s): unexpected msg - ~p",[emqttd_net:format(Peername), Msg]),
+handle_cast(Msg, State) ->
+    ?LOG(critical, "Unexpected msg: ~p", [Msg], State),
     noreply(State).
 
 handle_info(timeout, State) ->
-    stop({shutdown, timeout}, State);
+    shutdown(idle_timeout, State);
 
 handle_info({transaction, {timeout, Id}}, State) ->
     emqttd_stomp_transaction:timeout(Id),
     noreply(State);
 
-handle_info({heartbeat, start, {Cx, Cy}}, State = #state{transport = Transport, socket = Socket}) ->
+handle_info({heartbeat, start, {Cx, Cy}}, State = #stomp_client{connection = Connection}) ->
     Self = self(),
     Incomming = {Cx, statfun(recv_oct, State), fun() -> Self ! {heartbeat, timeout} end},
-    Outgoing  = {Cy, statfun(send_oct, State), fun() -> Transport:send(Socket, <<$\n>>) end},
+    Outgoing  = {Cy, statfun(send_oct, State), fun() -> Connection:send(<<$\n>>) end},
     {ok, HbProc} = emqttd_stomp_heartbeat:start_link(Incomming, Outgoing),
-    noreply(State#state{heartbeat = HbProc});
+    noreply(State#stomp_client{heartbeat = HbProc});
 
 handle_info({heartbeat, timeout}, State) ->
     stop({shutdown, heartbeat_timeout}, State);
 
-handle_info({'EXIT', HbProc, Error}, State = #state{heartbeat = HbProc}) ->
+handle_info({'EXIT', HbProc, Error}, State = #stomp_client{heartbeat = HbProc}) ->
     stop(Error, State);
+
+handle_info(activate_sock, State) ->
+    noreply(run_socket(State#stomp_client{conn_state = running}));
+
+handle_info({inet_async, _Sock, _Ref, {ok, Bytes}}, State) ->
+
+    ?LOG(debug, "RECV <- ~p", [Bytes], State),
+    received(Bytes, rate_limit(size(Data), State#stomp_client{await_recv = false}));
+
+handle_info({inet_async, _Sock, _Ref, {error, Reason}}, State) ->
+    shutdown(Reason, State);
 
 handle_info({inet_reply, _Ref, ok}, State) ->
     noreply(State);
 
-handle_info({inet_async, Sock, _Ref, {ok, Bytes}}, State = #state{peername = Peername, socket = Sock}) ->
-    lager:debug("RECV from ~s: ~p", [emqttd_net:format(Peername), Bytes]),
-    received(Bytes, control_throttle(State #state{await_recv = false}));
+handle_info({inet_reply, _Sock, {error, Reason}}, State) ->
+    shutdown(Reason, State);
 
-handle_info({inet_async, _Sock, _Ref, {error, Reason}}, State) ->
-    network_error(Reason, State);
-
-handle_info({inet_reply, _Sock, {error, Reason}}, State = #state{peername = Peername}) ->
-    lager:critical("Client ~s: unexpected inet_reply '~p'", [emqttd_net:format(Peername), Reason]),
-    {noreply, State};
-
-handle_info({dispatch, Msg}, State = #state{proto_state = ProtoState}) ->
+handle_info({dispatch, Msg}, State = #stomp_client{proto_state = ProtoState}) ->
     {ok, ProtoState1} = emqttd_stomp_proto:send(Msg, ProtoState),
-    {noreply, State#state{proto_state = ProtoState1}};
+    noreply(State#stomp_client{proto_state = ProtoState1});
 
-handle_info(Info, State = #state{peername = Peername}) ->
-    lager:critical("Stomp(~s): unexpected info ~p",[emqttd_net:format(Peername), Info]),
+handle_info(Info, State) ->
+    ?LOG(critical, "Unexpected info: ~p", [Info], State),
     {noreply, State}.
 
-terminate(Reason, #state{peername = Peername, proto_state = ProtoState}) ->
-    lager:info("Stomp(~s) terminated, reason: ~p", [emqttd_net:format(Peername), Reason]),
+terminate(Reason, State = #stomp_client{connection  = Connection,
+                                        proto_state = ProtoState}) ->
+    ?LOG(info, "terminated for ~p", [Reason], State),
+    Connection:fast_close(),
     case {ProtoState, Reason} of
-        {undefined, _} -> ok;
+        {undefined, _} ->
+            ok;
         {_, {shutdown, Error}} -> 
             emqttd_stomp_proto:shutdown(Error, ProtoState);
         {_,  Reason} ->
@@ -153,29 +182,61 @@ code_change(_OldVsn, State, _Extra) ->
 received(<<>>, State) ->
     noreply(State);
 
-received(Bytes, State = #state{conn_name   = ConnStr,
-                               parser      = Parser,
-                               proto_state = ProtoState}) ->
-    case Parser(Bytes) of
+received(Bytes, State = #stomp_client{parser_fun  = ParserFun,
+                                      proto_state = ProtoState}) ->
+    case catch ParserFun(Bytes) of
         {more, NewParser} ->
-            noreply(control_throttle(State#state{parser = NewParser}));
+            noreply(run_socket(State#stomp_client{parser_fun = NewParser}));
         {ok, Frame, Rest} ->
-            lager:info("RECV Frame: ~s", [emqttd_stomp_frame:format(Frame)]),
+            ?LOG(info, "RECV Frame: ~s", [emqttd_stomp_frame:format(Frame)], State),
             case emqttd_stomp_proto:received(Frame, ProtoState) of
                 {ok, ProtoState1}           ->
-                    received(Rest, reset_parser(State#state{proto_state = ProtoState1}));
+                    received(Rest, reset_parser(State#stomp_client{proto_state = ProtoState1}));
                 {error, Error, ProtoState1} ->
-                    stop({shutdown, Error}, State#state{proto_state = ProtoState1});
+                    shutdown(Error, State#stomp_client{proto_state = ProtoState1});
                 {stop, Reason, ProtoState1} ->
-                    stop(Reason, State#state{proto_state = ProtoState1})
+                    stop(Reason, State#stomp_client{proto_state = ProtoState1})
             end;
         {error, Error} ->
-            lager:error("Stomp(~s): framing error: ~s", [ConnStr, Error]),
-            stop({shutdown, frame_error}, State)
+            ?LOG(error, "Framing error - ~s", [Error], State),
+            shutdown(frame_error, State);
+        {'EXIT', Reason} ->
+            ?LOG(error, "Parser failed for ~p", [Reason], State),
+            ?LOG(error, "Error data: ~p", [Bytes], State),
+            shutdown(parser_error, State)
     end.
 
-reset_parser(State = #state{proto_env = ProtoEnv}) ->
-    State#state{parser = emqttd_stomp_frame:parser(ProtoEnv)}.
+reset_parser(State = #stomp_client{proto_env = ProtoEnv}) ->
+    State#stomp_client{parser_fun = emqttd_stomp_frame:parser(ProtoEnv)}.
+
+rate_limit(_Size, State = #stomp_client{rate_limit = undefined}) ->
+    run_socket(State);
+rate_limit(Size, State = #stomp_client{rate_limit = Limiter}) ->
+    case esockd_ratelimit:check(Limiter, Size) of
+        {0, Limiter1} ->
+            run_socket(State#stomp_client{conn_state = running,
+                                          rate_limit = Limiter1});
+        {Pause, Limiter1} ->
+            ?LOG(error, "Rate limiter pause for ~p", [Size, Pause], State),
+            erlang:send_after(Pause, self(), activate_sock),
+            State#stomp_client{conn_state = blocked, rate_limit = Limiter1}    
+    end.
+
+run_socket(State = #stomp_client{conn_state = blocked}) ->
+    State;
+run_socket(State = #stomp_client{await_recv = true}) ->
+    State;
+run_socket(State = #stomp_client{connection = Connection}) ->
+    Connection:async_recv(0, infinity),
+    State#stomp_client{await_recv = true}.
+
+statfun(Stat, #stomp_client{connection = Connection}) ->
+    fun() ->
+        case Connection:getstat([Stat]) of
+            {ok, [{Stat, Val}]} -> {ok, Val};
+            {error, Error}      -> {error, Error}
+        end
+    end.
 
 noreply(State) ->
     {noreply, State, hibernate}.
@@ -183,32 +244,6 @@ noreply(State) ->
 stop(Reason, State) ->
     {stop, Reason, State}.
 
-network_error(Reason, State = #state{peername = Peername}) ->
-    lager:warning("Stomp(~s): Network error '~p'", [emqttd_net:format(Peername), Reason]),
+shutdown(Reason, State) ->
     stop({shutdown, Reason}, State).
-
-control_throttle(State = #state{conn_state = Flow,
-                                conserve   = Conserve}) ->
-    case {Flow, Conserve} of
-        {running,   true} -> State #state{conn_state = blocked};
-        {blocked,  false} -> run_socket(State #state{conn_state = running});
-        {_,            _} -> run_socket(State)
-    end.
-
-run_socket(State = #state{conn_state = blocked}) ->
-    State;
-run_socket(State = #state{await_recv = true}) ->
-    State;
-run_socket(State = #state{transport = Transport, socket = Sock}) ->
-    Transport:async_recv(Sock, 0, infinity),
-    State#state{await_recv = true}.
-
-statfun(Stat, #state{transport = Transport, socket = Socket}) ->
-    fun() ->
-        case Transport:getstat(Socket, [Stat]) of
-            {ok, [{Stat, Val}]} -> {ok, Val};
-            {error, Error}      -> {error, Error}
-        end
-    end.
-
 
