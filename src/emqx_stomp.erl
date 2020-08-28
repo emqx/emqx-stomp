4b752025144b07c5319233c1c7bb4d95ec8d46b5
JianBo He <heeejianbo@gmail.com>
2020-08-28 10:42:43 +0800

    refactor(listener): refactor listenter started funcs

--- src/emqx_stomp.erl
+++ src/emqx_stomp.erl
@@ -25,8 +25,12 @@
         , stop/1
         ]).
 
--export([ start_listener/0
-        , stop_listener/0
+-export([ start_listeners/0
+        , start_listener/1
+        , start_listener/3
+        , stop_listeners/0
+        , stop_listener/1
+        , stop_listener/3
         ]).
 
 -export([init/1]).
@@ -34,17 +38,19 @@
 -define(APP, ?MODULE).
 -define(TCP_OPTS, [binary, {packet, raw}, {reuseaddr, true}, {nodelay, true}]).
 
+-type(listener() :: {esockd:proto(), esockd:listen_on(), [esockd:option()]}).
+
 %%--------------------------------------------------------------------
 %% Application callbacks
 %%--------------------------------------------------------------------
 
 start(_StartType, _StartArgs) ->
     {ok, Sup} = supervisor:start_link({local, emqx_stomp_sup}, ?MODULE, []),
-    start_listener(),
+    start_listeners(),
     {ok, Sup}.
 
 stop(_State) ->
-    stop_listener().
+    stop_listeners().
 
 %%--------------------------------------------------------------------
 %% Supervisor callbacks
@@ -57,18 +63,77 @@ init([]) ->
 %% Start/Stop listeners
 %%--------------------------------------------------------------------
 
-start_listener() ->
+-spec(start_listeners() -> ok).
+start_listeners() ->
+    lists:foreach(fun start_listener/1, listeners_confs()).
+
+-spec(start_listener(listener()) -> ok).
+start_listener({Proto, ListenOn, Options}) ->
+    case start_listener(Proto, ListenOn, Options) of
+        {ok, _} -> io:format("Start stomp:~s listener on ~s successfully.~n",
+                             [Proto, format(ListenOn)]);
+        {error, Reason} ->
+            io:format(standard_error, "Failed to start stomp:~s listener on ~s - ~0p~n!",
+                      [Proto, format(ListenOn), Reason]),
+            error(Reason)
+    end.
+
+-spec(start_listener(esockd:proto(), esockd:listen_on(), [esockd:option()])
+      -> {ok, pid()} | {error, term()}).
+start_listener(tcp, ListenOn, Options) ->
+    start_stomp_listener('stomp:tcp', ListenOn, Options);
+start_listener(ssl, ListenOn, Options) ->
+    start_stomp_listener('stomp:ssl', ListenOn, Options).
+
+%% @private
+start_stomp_listener(Name, ListenOn, Options) ->
+    SockOpts = esockd:parse_opt(Options),
+    esockd:open(Name, ListenOn, merge_default(SockOpts),
+                 {emqx_stomp_connection, start_link, [Options -- SockOpts]}).
+
+-spec(stop_listeners() -> ok).
+stop_listeners() ->
+    lists:foreach(fun stop_listener/1, listeners_confs()).
+
+-spec(stop_listener(listener()) -> ok | {error, term()}).
+stop_listener({Proto, ListenOn, Opts}) ->
+    StopRet = stop_listener(Proto, ListenOn, Opts),
+    case StopRet of
+        ok -> io:format("Stop stomp:~s listener on ~s successfully.~n",
+                        [Proto, format(ListenOn)]);
+        {error, Reason} ->
+            io:format(standard_error, "Failed to stop stomp:~s listener on ~s - ~p~n.",
+                      [Proto, format(ListenOn), Reason])
+    end,
+    StopRet.
+
+-spec(stop_listener(esockd:proto(), esockd:listen_on(), [esockd:option()])
+      -> ok | {error, term()}).
+stop_listener(tcp, ListenOn, _Opts) ->
+    esockd:close('stomp:tcp', ListenOn);
+stop_listener(ssl, ListenOn, _Opts) ->
+    esockd:close('stomp:ssl', ListenOn).
+
+%%--------------------------------------------------------------------
+%% Internal funcs
+%%--------------------------------------------------------------------
+
+listeners_confs() ->
     {ok, {Port, Opts}} = application:get_env(?APP, listener),
-    {ok, Env} = application:get_env(?APP, frame),
-    MFArgs = {emqx_stomp_connection, start_link, [Env]},
-    esockd:open(stomp, Port, merge_opts(Opts), MFArgs).
+    Options = application:get_env(?APP, frame, []),
+    [{tcp, Port, Options ++ Opts}].
 
-merge_opts(Opts) ->
-    TcpOpts = emqx_misc:merge_opts(
-                 ?TCP_OPTS, proplists:get_value(tcp_options, Opts, [])),
-    emqx_misc:merge_opts(Opts, [{tcp_options, TcpOpts}]).
+merge_default(Options) ->
+    case lists:keytake(tcp_options, 1, Options) of
+        {value, {tcp_options, TcpOpts}, Options1} ->
+            [{tcp_options, emqx_misc:merge_opts(?TCP_OPTS, TcpOpts)} | Options1];
+        false ->
+            [{tcp_options, ?TCP_OPTS} | Options]
+    end.
 
-stop_listener() ->
-    {ok, {Port, _Opts}} = application:get_env(?APP, listener),
-    esockd:close({stomp, Port}).
-
+format(Port) when is_integer(Port) ->
+    io_lib:format("0.0.0.0:~w", [Port]);
+format({Addr, Port}) when is_list(Addr) ->
+    io_lib:format("~s:~w", [Addr, Port]);
+format({Addr, Port}) when is_tuple(Addr) ->
+    io_lib:format("~s:~w", [inet:ntoa(Addr), Port]).
